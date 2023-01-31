241b61197911c406acf9ef76ffeaf7772ec89f35
RowanElsworth <rowanelsworth@gmail.com>
2023-01-31 18:22:29 +0000

    Pre git page deploy

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from './pages/About/About.jsx';
+import Portfolio from './pages/Portfolio/Portfolio'
+import Careers from './pages/Careers/Careers';
+import Contact from './pages/Contact/Contact';
+import Recources from './pages/Recources/Recources'
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="About" element={<About />} />
+        <Route path="Portfolio" element={<Portfolio />} />
+        <Route path="Careers" element={<Careers />} />
+        <Route path="Contact" element={<Contact />} />
+        <Route path="Recources" element={<Recources />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
--- src/index.js
+++ src/index.js
@@ -1,27 +1,17 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Portfolio from './pages/Portfolio/Portfolio'
-import Careers from './pages/Careers/Careers';
-import Contact from './pages/Contact/Contact';
-import Recources from './pages/Recources/Recources'
-
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="About" element={<About />} />
-        <Route path="Portfolio" element={<Portfolio />} />
-        <Route path="Careers" element={<Careers />} />
-        <Route path="Contact" element={<Contact />} />
-        <Route path="Recources" element={<Recources />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+import './index.scss';
+import App from './app';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
