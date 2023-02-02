19efdbebf4dcd20dce8825969a60659a3bd3b2b0
RowanElsworth <rowanelsworth@gmail.com>
2023-02-02 02:15:14 +0000

    react router now scrolls to top

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './ScrollToTop';
 
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
@@ -11,6 +12,7 @@ import Recources from './pages/Recources/Recources';
 export default function App() {
   return (
     <BrowserRouter>
+    <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="About" element={<About />} />
