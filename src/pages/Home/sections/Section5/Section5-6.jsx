a739ee4b15a4c6a45c3e62a80b92cc942cc92c61
RowanElsworth <rowanelsworth@gmail.com>
2023-02-26 22:06:05 +0000

    near home page completion (pretty sure I've said that before) just need to sort out globey on the bottom, some of the content and some content is overflowing to other sections

--- src/pages/Home/sections/Section5/Section5-6.jsx
+++ src/pages/Home/sections/Section5/Section5-6.jsx
@@ -1,4 +1,6 @@
 
+import { useState, useEffect } from "react";
+
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper';
@@ -13,26 +15,68 @@ import './Clouds.scss'
 import './Section5-6.scss'
 
 export function Section5() {
+  // Define state for clouds array and function to update it
+  const [clouds, setClouds] = useState([]);
+
+  // Define function to generate a new cloud object with randomized properties
+  const generateCloud = () => {
+    // Randomize cloud size between 10% and 30%
+    const size = Math.floor(Math.random() * 21) + 10;
+    // Randomize y axis entry position between 0% and 65%
+    const yPosition = Math.floor(Math.random() * 66);
+    // Randomize animation duration between 12 and 22 seconds
+    const duration = Math.floor(Math.random() * 11) + 12;
+
+    // Return new cloud object with randomized properties
+    return {
+      id: Date.now(),
+      size: `${size}%`,
+      yPosition: `${yPosition}%`,
+      duration: `${duration}s`,
+    };
+  };
+
+  // Set up interval to generate new clouds on a randomized interval
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newCloud = generateCloud();
+      // Add new cloud to state using spread operator
+      setClouds((prevClouds) => [...prevClouds, newCloud]);
+    }, Math.floor(Math.random() * 2000) + 3000);
+
+    // Clean up interval when component unmounts
+    return () => clearInterval(interval);
+  }, []);
+
+  // Define function to remove cloud from state by ID
+  const deleteCloud = (id) => {
+    setClouds((prevClouds) => prevClouds.filter((cloud) => cloud.id !== id));
+  };
+
+  // Generate first cloud on mount
+  useEffect(() => {
+    const newCloud = generateCloud();
+    // Set initial state to array with first cloud
+    setClouds([newCloud]);
+  }, []);
+
   return (
     <section className="clouds">
-      <div class="initialCloud">
-        <svg id="svgCloud" data-name="clouder" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 348 164"><defs><style></style></defs><path class="cloud1Fill" d="M348,107.5a54.5,54.5,0,0,1-94.87,36.61,77.55,77.55,0,0,1-81.57-1.43A73,73,0,0,1,71,145.07,42.48,42.48,0,1,1,49.61,71.59,73,73,0,0,1,154.85,26.84,77.51,77.51,0,0,1,287.16,53.37,53,53,0,0,1,293.5,53,54.5,54.5,0,0,1,348,107.5Z"/></svg>
-      </div>
-      <div class="firstCloud">
-        <svg id="svgCloud" data-name="clouder" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 348 164"><defs><style></style></defs><path class="cloud1Fill" d="M348,107.5a54.5,54.5,0,0,1-94.87,36.61,77.55,77.55,0,0,1-81.57-1.43A73,73,0,0,1,71,145.07,42.48,42.48,0,1,1,49.61,71.59,73,73,0,0,1,154.85,26.84,77.51,77.51,0,0,1,287.16,53.37,53,53,0,0,1,293.5,53,54.5,54.5,0,0,1,348,107.5Z"/></svg>
-      </div>
-      <div class="secondCloud">
-        <svg id="svgCloud" data-name="clouder" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 348 164"><defs><style></style></defs><path class="cloud1Fill" d="M348,107.5a54.5,54.5,0,0,1-94.87,36.61,77.55,77.55,0,0,1-81.57-1.43A73,73,0,0,1,71,145.07,42.48,42.48,0,1,1,49.61,71.59,73,73,0,0,1,154.85,26.84,77.51,77.51,0,0,1,287.16,53.37,53,53,0,0,1,293.5,53,54.5,54.5,0,0,1,348,107.5Z"/></svg>
-      </div>
-      <div class="thirdCloud">
-        <svg id="svgCloud" data-name="clouder" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 348 164"><defs><style></style></defs><path class="cloud1Fill" d="M348,107.5a54.5,54.5,0,0,1-94.87,36.61,77.55,77.55,0,0,1-81.57-1.43A73,73,0,0,1,71,145.07,42.48,42.48,0,1,1,49.61,71.59,73,73,0,0,1,154.85,26.84,77.51,77.51,0,0,1,287.16,53.37,53,53,0,0,1,293.5,53,54.5,54.5,0,0,1,348,107.5Z"/></svg>
-      </div>
-      <div class="fourthCloud">
-        <svg id="svgCloud" data-name="clouder" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 348 164"><defs><style></style></defs><path class="cloud1Fill" d="M348,107.5a54.5,54.5,0,0,1-94.87,36.61,77.55,77.55,0,0,1-81.57-1.43A73,73,0,0,1,71,145.07,42.48,42.48,0,1,1,49.61,71.59,73,73,0,0,1,154.85,26.84,77.51,77.51,0,0,1,287.16,53.37,53,53,0,0,1,293.5,53,54.5,54.5,0,0,1,348,107.5Z"/></svg>
-      </div>
-      <div class="fifthCloud">
-        <svg id="svgCloud" data-name="clouder" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 348 164"><defs><style></style></defs><path class="cloud1Fill" d="M348,107.5a54.5,54.5,0,0,1-94.87,36.61,77.55,77.55,0,0,1-81.57-1.43A73,73,0,0,1,71,145.07,42.48,42.48,0,1,1,49.61,71.59,73,73,0,0,1,154.85,26.84,77.51,77.51,0,0,1,287.16,53.37,53,53,0,0,1,293.5,53,54.5,54.5,0,0,1,348,107.5Z"/></svg>
-      </div>
+      {clouds.map((cloud) => (
+        <div
+          key={cloud.id}
+          className="cloud"
+          style={{
+            width: cloud.size,
+            height: "auto",
+            top: cloud.yPosition,
+            animationDuration: cloud.duration,
+          }}
+          onAnimationEnd={() => deleteCloud(cloud.id)}
+        >
+          <svg id="svgCloud" data-name="clouder" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 348 164"><defs><style></style></defs><path class="cloud1Fill" d="M348,107.5a54.5,54.5,0,0,1-94.87,36.61,77.55,77.55,0,0,1-81.57-1.43A73,73,0,0,1,71,145.07,42.48,42.48,0,1,1,49.61,71.59,73,73,0,0,1,154.85,26.84,77.51,77.51,0,0,1,287.16,53.37,53,53,0,0,1,293.5,53,54.5,54.5,0,0,1,348,107.5Z"/></svg>
+        </div>
+      ))}
       <Swiper
         navigation={true}
         slidesPerView={1.05}
@@ -58,7 +102,7 @@ export function Section5() {
         <SwiperSlide>
           <a className="article-1" href="https://www.hswsolutions.com/resources/why-a-mobile-friendly-website-is-important/" rel="noreferrer" target="_blank">
             <div className="bottom-container">
-              <p>Why your website must be mobile-friendly</p>
+              <p className="article-title">Why your website must be mobile-friendly</p>
               <p className="view-more">View</p>
             </div>
           </a>
@@ -66,7 +110,7 @@ export function Section5() {
         <SwiperSlide>
           <a className="article-2" href="https://www.redlineminds.com/search-engine-optimizaton/what-is-seo-how-to-do-seo-for-ecommerce/" rel="noreferrer" target="_blank">
             <div className="bottom-container">
-              <p>What is SEO?</p>
+              <p className="article-title">What is SEO?</p>
               <p className="view-more">View</p>
             </div>
           </a>
@@ -74,7 +118,7 @@ export function Section5() {
         <SwiperSlide>
           <a className="article-3" href="https://www.digitaldoughnut.com/articles/2020/december-2020/why-unique-content-is-more-important-for-your-site" rel="noreferrer" target="_blank">
             <div className="bottom-container">
-              <p>Does your website need to be unique?</p>
+              <p className="article-title">Does your website need to be unique?</p>
               <p className="view-more">View</p>
             </div>
           </a>
@@ -82,7 +126,7 @@ export function Section5() {
         <SwiperSlide>
           <a className="article-4" href="https://www.glasgowcreative.co.uk/why-colour-scheme-is-important-branding-business/" rel="noreferrer" target="_blank">
             <div className="bottom-container">
-              <p>Benefits of a consistent colour scheme</p>
+              <p className="article-title">Benefits of a consistent colour scheme</p>
               <p className="view-more">View</p>
             </div>
           </a>
@@ -90,7 +134,7 @@ export function Section5() {
         <SwiperSlide>
           <a className="article-5" href="https://www.dreamhost.com/blog/website-benefits-for-small-business/" rel="noreferrer" target="_blank">
             <div className="bottom-container">
-              <p>Why websites are good for small businesses</p>
+              <p className="article-title">Why websites are good for small businesses</p>
               <p className="view-more">View</p>
             </div>
           </a>
@@ -98,7 +142,7 @@ export function Section5() {
         <SwiperSlide>
           <a className="article-6" href="https://www.microdinc.com/blog/improve-customer-satisfaction/" rel="noreferrer" target="_blank">
             <div className="bottom-container">
-              <p>How websites boost customer satisfaction</p>
+              <p className="article-title">How websites boost customer satisfaction</p>
               <p className="view-more">View</p>
             </div>
           </a>
@@ -106,7 +150,7 @@ export function Section5() {
         <SwiperSlide>
           <a className="article-7" href="https://ballywiremedia.ie/2021/10/05/1580-2/" rel="noreferrer" target="_blank">
             <div className="bottom-container">
-              <p>Benefits of websites over traditional media</p>
+              <p className="article-title">Benefits of websites over traditional media</p>
               <p className="view-more">View</p>
             </div>
           </a>
@@ -114,14 +158,12 @@ export function Section5() {
       </Swiper>
 
       {/* section 6 - convinced */}
-      {/* might want to attach the gif and globey into one image/gif instead of fucking around with sizing */}
       <section className="convinced">
         <div className="globey">
           <img src={globey} alt="" />
         </div>
         <div className="content-wrapper">
           <img src={hotBeansGif} alt="" className='hot-beans-gif'/>
-          <p className="convinced-text">Convinced?</p>
           <ContactBtn className="s6-contact"></ContactBtn>
         </div>
       </section>
